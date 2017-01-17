c4833fd7e96ad146815d850603b044b7d2f8c86e
USER <USER@USER8-PC>
2017-01-17 09:46:21 +0900

    bbb

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class Calculator {
 	
-	public int add(int num1,int num2){
+	public int add1(int num1,int num2){
 		int result=0;
 		result=num1+ num2;
 		return result;
