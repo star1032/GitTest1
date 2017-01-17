ae176c238f17d5e9a0f7ccb857df138f43229c2a
USER <USER@USER8-PC>
2017-01-17 10:15:25 +0900

    Complete Calculator

--- calculator/src/calculator/Calculator.java
+++ calculator/src/calculator/Calculator.java
@@ -12,5 +12,15 @@ public class Calculator {
 		result=num1 - num2;
 		return result;
 	}
+	public int mul(int num1,int num2){
+		int result=0;
+		result=num1 * num2;
+		return result;
+	}
+	public int div(int num1,int num2){
+		int result=0;
+		result=num1 / num2;
+		return result;
+	}
 
 }
